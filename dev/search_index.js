73ebcc9ee3d3ec43b1f61e9074b7350a6e86f703
Documenter.jl <documenter@juliadocs.github.io>
2025-01-27 14:36:50 +0000

    build based on a7a5b24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GnssCore","category":"page"},{"location":"#GnssCore","page":"Home","title":"GnssCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GnssCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GnssCore]","category":"page"}]
+}
