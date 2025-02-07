d69126954411e3ce47a6e1b4b07318694f029ee0
Fernando Conde-Pumpido <fernando.conde.pumpido@outlook.com>
2025-02-07 17:54:39 +0100

    Initial code to begin joint development with GnssNavipedia.jl

--- src/GnssCore.jl
+++ src/GnssCore.jl
@@ -1,5 +1,36 @@
 module GnssCore
 
-# Write your package code here.
+# navipedia supertype
+abstract type AbstractNavipedia end
+
+# ADTs supertypes
+abstract type AbstractConstellation end
+abstract type AbstractSignalBand end
+abstract type AbstractSignalComponent end
+
+# ADT getters
+"""
+    get_constellations(n::AbstractNavipedia)::Module
+
+Retrieve constellation ADT module available in navipedia `n`.
+"""
+get_constellations(::AbstractNavipedia)::Module
+
+"""
+    get_signal_bands(n::AbstractNavipedia)::Module
+
+Retrieve signal bands ADT module available in navipedia `n`.
+"""
+get_signal_bands(::AbstractNavipedia)::Module
+
+"""
+    get_signal_types(n::AbstractNavipedia)::Module
+
+Retrieve signal component ADT module available in navipedia `n`.
+"""
+get_signal_types(::AbstractNavipedia)::Module
+
+
+# TODO add more generic interfaces
 
 end
